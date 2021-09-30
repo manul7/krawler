48813745a27c9bf2e6d5b53ca8e011beabd88740
Konstantin Belov <belov@arrival.com>
2021-09-30 15:56:59 +0300

    feat: initial commit

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+import logging
+
+__author__ = 'Konstantin Belov'
+__version__ = '0.1.0'
+
+logging.getLogger('crawler').addHandler(logging.NullHandler())
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from app import __author__, __version__
+
+setup(
+    name='HttpCrawler',
+    version=__version__,
+    # license="Proprietary License",
+    description="Test app",
+    # platforms='any',
+    keywords="http crawler test".split(), # noqa
+    author=__author__,
+    author_email='konstantin.belov@gmail.com',
+    # url='http://TBD',
+    classifiers=[
+        'Development Status :: 4 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['app'],
+    include_package_data=True,
+    install_requires=[
+        'pytz',
+        'six',
+        'requests',
+        'beautifulsoup4',
+        'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'krawl = app.crawl:cli'
+        ]
+    },
+)
