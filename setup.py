7b8a13dec48f4114ccae4416fa687b1ef8ebb566
Konstantin Belov <belov@arrival.com>
2021-10-06 00:45:15 +0300

    refactor

--- setup.py
+++ setup.py
@@ -2,37 +2,27 @@ from setuptools import setup
 from app import __author__, __version__
 
 setup(
-    name='HttpCrawler',
+    name="HttpCrawler",
     version=__version__,
     # license="Proprietary License",
     description="Test app",
     # platforms='any',
-    keywords="http crawler test".split(), # noqa
+    keywords="http crawler test".split(),  # noqa
     author=__author__,
-    author_email='konstantin.belov@gmail.com',
+    author_email="konstantin.belov@gmail.com",
     # url='http://TBD',
     classifiers=[
-        'Development Status :: 4 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: Other/Proprietary License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
     ],
-    packages=['app'],
+    packages=["app"],
     include_package_data=True,
-    install_requires=[
-        'pytz',
-        'six',
-        'requests',
-        'beautifulsoup4',
-        'click'
-    ],
-    entry_points={
-        'console_scripts': [
-            'krawl = app.crawler:cli'
-        ]
-    },
+    install_requires=["pytz", "six", "requests", "beautifulsoup4", "click"],
+    entry_points={"console_scripts": ["krawl = app.crawler:cli"]},
 )
--- tests/test_checks.py
+++ tests/test_checks.py
@@ -2,12 +2,15 @@ import pytest
 import app.checks
 
 
-@pytest.mark.parametrize("url, exp", [
-    ("http://www.cwi.nl:80/%7Eguido/Python.html", True),
-    ("/data/Python.html'", False),
-    ("39", False),
-    (u'assdd3', False),
-    ('https://stackoverflow.com', True),
-    ])
+@pytest.mark.parametrize(
+    "url, exp",
+    [
+        ("http://www.cwi.nl:80/%7Eguido/Python.html", True),
+        ("/data/Python.html'", False),
+        ("39", False),
+        (u"assdd3", False),
+        ("https://stackoverflow.com", True),
+    ],
+)
 def test_is_url_valud(url, exp):
     assert app.checks.is_url_valid(url) is exp
