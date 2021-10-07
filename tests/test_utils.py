c97cee5a21b91a3e0ffd349e2d183899e6066478
Konstantin Belov <belov@arrival.com>
2021-10-07 12:49:40 +0300

    tests: added test for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+import pytest
+from app.utils import *
+
+
+@pytest.mark.parametrize(
+    "base, url, exp",
+    [
+        (None, "http://www.cwi.nl", "http://www.cwi.nl"),
+        ("NOTNONE", "http://www.cwi.nl", "http://www.cwi.nl"),
+        ("Base", "/data/index.html", "http://Base/data/index.html"),
+        ("BBB", "dk", "http://BBB/dk"),
+        ("BBB", "/dk/", "http://BBB/dk/")
+    ],
+)
+def test_expand_url(base, url, exp):
+    assert exp == expand_url(base, url)
